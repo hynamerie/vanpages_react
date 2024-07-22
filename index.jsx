6cc4232ac067f977168c6fb20ddad8f73d6d298a
amerihn <amerihn.art@gmail.com>
2024-07-23 02:36:46 +0700

    Revert "up"
    This reverts commit 0f905ca42d97de371bfcaf2b0bb53e54b4630845.

--- index.jsx
+++ index.jsx
@@ -16,8 +16,6 @@ import HostVanDetail from './pages/Host/HostVanDetail';
 import HostVanInfo from './pages/Host/HostVanInfo';
 import HostVanPricing from './pages/Host/HostVanPricing';
 import HostVanPhotos from './pages/Host/HostVanPhotos';
-import AuthRequired from './components/AuthRequired';
-import Login from './pages/Login';
 
 
 function App() {
@@ -30,21 +28,19 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          <Route path="login" element={<Login />} />
           
-          <Route element={<AuthRequired/>}>
-            <Route path="host" element={<HostLayout/>}>
-              <Route index element={<Dashboard />} />
-              <Route path="income" element={<Income />} />
-              <Route path="reviews" element={<Reviews />} />
-              <Route path='vans' element={<HostVans/>} />
-              
-              <Route path='vans/:id' element={<HostVanDetail/>}>
-                <Route index element={<HostVanInfo />} />
-                <Route path='pricing' element={<HostVanPricing />}/>
-                <Route path='photos' element={<HostVanPhotos />}/>
-              </Route>
-          </Route>
+          <Route path="host" element={<HostLayout/>}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path='vans' element={<HostVans/>} />
+            
+            <Route path='vans/:id' element={<HostVanDetail/>}>
+              <Route index element={<HostVanInfo />} />
+              <Route path='pricing' element={<HostVanPricing />}/>
+              <Route path='photos' element={<HostVanPhotos />}/>
+            </Route>
+
           </Route>
           <Route path="*" element={<h1>Page not found!</h1>} />
         </Route>

192c162c2c50e1bd0180cc1b7f73753da237bfbb
amerihn <amerihn.art@gmail.com>
2024-07-24 16:35:40 +0700

    up

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { BsStarFill } from "react-icons/bs"
 
 export default function Dashboard() {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <section className="host-dashboard-earnings">
+          <div className="info">
+              <h1>Welcome!</h1>
+              <p>Income last <span>30 days</span></p>
+              <h2>$2,260</h2>
+          </div>
+          <Link to="income">Details</Link>
+      </section>
+
+      <section className="host-dashboard-reviews">
+          <h2>Review score</h2>
+
+          <BsStarFill className="star" />
+
+          <p>
+              <span>5.0</span>/5
+          </p>
+          <Link to="reviews">Details</Link>
+      </section>
+
+      <section className="host-dashboard-vans">
+          <div className="top">
+              <h2>Your listed vans</h2>
+              <Link to="vans">View all</Link>
+          </div>
+      </section>
+    </>
   )
 }
--- pages/Host/Reviews.jsx
+++ pages/Host/Reviews.jsx
@@ -6,14 +6,14 @@ export default function Reviews() {
         {
             rating: 5,
             name: "Elliot",
-            date: "January 3, 2023",
+            date: "January 3, 2024",
             text: "The beach bum is such an awesome van! Such a comfortable trip. We had it for 2 weeks and there was not a single issue. Super clean when we picked it up and the host is very comfortable and understanding. Highly recommend!",
             id: "1",
         },
         {
             rating: 5,
             name: "Sandy",
-            date: "December 12, 2022",
+            date: "December 12, 2023",
             text: "This is our third time using the Modest Explorer for our travels and we love it! No complaints, absolutely perfect!",
             id: "2",
         },
